3afde7c1cd2149a5b5059822e07e026f708ebcee
varshithkumarylf47-rgb <varshithkumar.ylf47@gmail.com>
2025-10-11 23:14:27 -0400

    Updated

--- com/training/scan/SquareRoot.java
+++ com/training/scan/SquareRoot.java
@@ -5,20 +5,23 @@ import java.util.Scanner;
 public class SquareRoot {
     public static void main(String[] args) {
         double[] numbers = new double[3];
+        System.out.print("Enter the numbers: ");
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = scanner.nextInt();
         }
-        System.out.print("Entered numbers: ");
+        System.out.print("Entered array: ");
         for (double number : numbers)
             System.out.print(number + " ");
         System.out.println();
-        System.out.print("Square root of the arrays entered above in a new array: ");
         double[] squareRootNumbers = new double[3];
 
         for (int i = 0; i < numbers.length; i++) {
             squareRootNumbers[i] = Math.sqrt(numbers[i]);
-            System.out.print(squareRootNumbers[i]+ " ");
+        }
+        System.out.print("Square root of the arrays entered above in a new array: ");
+        for( double rootNumber : squareRootNumbers ){
+            System.out.print(rootNumber+" ");
         }
 
     }
