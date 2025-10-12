35dce80e8c45c8a6c9df04962df2a4077217fcca
varshithkumarylf47-rgb <varshithkumar.ylf47@gmail.com>
2025-10-11 20:27:26 -0400

    Updated

--- com/training/scan/Login.java
+++ com/training/scan/Login.java
@@ -5,17 +5,20 @@ import java.util.Scanner;
 
 public class Login {
     public static void main(String[] args) {
-        ArrayList<String> userNames = new ArrayList<String>();
-        userNames.add("shivani21");
-        userNames.add("arun05");
-        userNames.add("shailaja19");
-        userNames.add("varshith27");
+        String[] userNames = {"shivani21", "arun05", "shailaja19", "varshith27"};
         System.out.print("Enter username to login : ");
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
-        String newName = scanner.next();
-        if(userNames.contains(newName))
-            System.out.println("You are logged in successfully");
-        else
+        String userName = scanner.next();
+        for (String name : userNames) {
+            if (userName.equals(name)) {
+                System.out.println("You are logged in successfully");
+                count++;
+                break;
+            }
+        }
+        if (count == 0) {
             System.out.println("Invalid username");
+        }
     }
 }
