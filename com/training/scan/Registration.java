bd59eb1b2796453dd47bba4731294289d53da73a
varshithkumarylf47-rgb <varshithkumar.ylf47@gmail.com>
2025-10-11 20:28:26 -0400

    Updated

--- com/training/scan/Registration.java
+++ com/training/scan/Registration.java
@@ -1,25 +1,23 @@
 package com.training.scan;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Registration {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Shivani");
-        names.add("Arun");
-        names.add("Shailaja");
-        names.add("Varshith");
+        String[] names = {"Shivani", "Arun", "Shailaja", "Varshith"};
         System.out.print("Enter name to register : ");
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
         String newName = scanner.next();
-        if (names.contains(newName))
+        for (String name : names) {
+            if (newName.equals(name)) {
+                System.out.println("Name is not unique");
+                count++;
+                break;
+            }
+        }
+        if (count == 0) {
             System.out.println("You are registered");
-        else
-            System.out.println("Name is not unique");
+        }
     }
-
-
 }
-
-
