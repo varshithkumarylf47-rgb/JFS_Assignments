325cf769c4c47712c74cc7eea184b492c931cbc8
varshithkumarylf47-rgb <varshithkumar.ylf47@gmail.com>
2025-12-06 01:41:07 -0500

    Interface with updated methods

---
+++ com/bookappStreams/service/IBookService.java
@@ -0,0 +1,28 @@
+package com.bookappStreams.service;
+
+
+
+import com.bookappStreams.exception.BookNotFoundException;
+import com.bookappStreams.model.Book;
+
+import java.util.List;
+
+public interface IBookService {
+    List<Book> getAll();
+
+    List<Book> getByAuthorContains(String author) throws BookNotFoundException;
+
+    List<Book> getByCategory(String category) throws BookNotFoundException;
+
+    int getCountOfBooksByAuthor(String author)throws BookNotFoundException;
+
+    List<String> getByPriceLessThan(double price) throws BookNotFoundException;
+
+    List<Book> getByDatePublished(int year) throws BookNotFoundException;
+
+    List<Book> getByAuthorContainsAndCategory(String author, String category) throws BookNotFoundException;
+
+    Book getById(int bookId) throws BookNotFoundException;
+
+    double getTotalPrice(String category);
+}
